6ab3e6435daf18b63a9443a9ad557469a5792284
Ge-go <821230693@qq.com>
2022-09-20 20:50:06 +0800

    2022/09/20

--- wx/miniprogram/app.ts
+++ wx/miniprogram/app.ts
@@ -1,44 +1,27 @@
 import { IAppOption } from "./appoption"
 
+let resolveUserInfo: (value: WechatMiniprogram.UserInfo | PromiseLike<WechatMiniprogram.UserInfo>) => void
+let rejectUserInfo: (reason?: any) => void
+
 // app.ts
 App<IAppOption>({
   globalData: {
-    
-  },
-  onLaunch() {
-    // 请求
-    wx.request({
-      url: 'http://localhost:8080/trip/trip123',
-      method: 'GET',
-      success: console.log,
-      fail: console.error,
+    userInfo: new Promise((resolve, reject) => {
+      resolveUserInfo = resolve
+      rejectUserInfo = reject
     })
-
+  },
+  async onLaunch() {
     // 登录
-    wx.login({
-      success: res => {
-        console.log(res.code)
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
 
-      },
-    })
+    // 获取用户信息
+    try {
 
-    // 获取用户信息,不使用回调函数
-    // wx.getSetting().then(
-    //   res => {
-    //     if (res.authSetting['scope.userInfo']) {
-    //       return wx.getUserInfo()
-    //     }
-    //     return undefined
-    //   }).then(res => {
-    //     if (!res) {
-    //       return
-    //     }
-    //     this.globalData.userInfo = res.userInfo
-    //     //通知页面
-    //     if (this.userInfoReadyCallback) {
-    //       this.userInfoReadyCallback(res)
-    //     }
-    //   })
+    } catch (err) {
+      rejectUserInfo(err)
+    }
   },
+  resolveUserInfo(userInfo: WechatMiniprogram.UserInfo) {
+    resolveUserInfo(userInfo)
+  }
 })
