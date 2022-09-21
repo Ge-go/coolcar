67a716c999752d52e4972ad6ab47f68736fed6be
C_wangchenguang <chenguang.wang2@pactera.com>
2022-09-21 14:07:21 +0800

    转移部分无用代码

--- README.md
+++ README.md
@@ -19,14 +19,21 @@ This is a car rental applet front-end code.
         utils     //维护路由,和自定义各种实现的api方法
 
     server:服务后台
-        proto  pb协议文件
-
     admin:后台管理
     deployment:部署脚本 (k8s配置)
     miniprogram:前端工作目录(目前小程序内置ts转js)
+    architectureDiagram:架构图存放位置
+    extra:部分原来的代码存放地
 
 ## 如何编译以及运行小程序
-    cd wx
+    cd wx/miniprogram
     npm install
     点击工具构建npm
     点击编译
+
+## 暂存问题
+    bat命令执行问题
+    可能会少执行一条pbts命令,需要手动执行
+    生成的.d.ts头文件要复制到js中
+
+![微服务架构图](../coolcar/architectureDiagram/wfw.png)
--- wx/miniprogram/app.js
+++ wx/miniprogram/app.js
@@ -1,5 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const camelcase_keys_1 = __importDefault(require("camelcase-keys"));
+const trip_pb_1 = require("./service/proto_gen/trip_pb");
 let resolveUserInfo;
 let rejectUserInfo;
 // app.ts
@@ -11,6 +16,16 @@ App({
         })
     },
     async onLaunch() {
+        // wx.request({
+        //     url: 'http://localhost:8080/trip/trip123',
+        //     method: 'GET',
+        //     success: res => {
+        //         const getTripRes = trip_pb_1.coolcar.GetTripRsp.fromObject(camelcase_keys_1.default(res.data, {
+        //             deep: true,
+        //         }));
+        //         console.log(getTripRes);
+        //     }
+        // });
         // 登录
         // 获取用户信息
         try {
