8ed61c73185837d63b0cbbad5ef39af4fb8ac49f
Ge-go <821230693@qq.com>
2022-09-26 21:41:09 +0800

    完善updatetrip,并写出测试代码,但并未进行测试,因为本地尚无docker服务

--- server/rental/trip/trip.go
+++ server/rental/trip/trip.go
@@ -3,11 +3,17 @@ package trip
 import (
 	"context"
 	rentalpb "coolcar/rental/api/gen/v1"
+	"coolcar/rental/trip/dao"
+	"coolcar/shared/auth"
+	"coolcar/shared/id"
 	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Service struct {
 	Log *zap.Logger
+	col *dao.Mongo
 }
 
 func (s *Service) CreateTrip(ctx context.Context, req *rentalpb.CreateTripReq) (*rentalpb.TripEntity, error) {
@@ -23,5 +29,21 @@ func (s *Service) GetTrips(ctx context.Context, req *rentalpb.GetTripsReq) (*ren
 }
 
 func (s *Service) UpdateTrip(ctx context.Context, req *rentalpb.UpdateTripReq) (*rentalpb.Trip, error) {
-	panic("implement me")
+	accountID, err := auth.AccountIDFromContext(ctx)
+	if err != nil {
+		return nil, status.Errorf(codes.Unauthenticated, "")
+	}
+	trip, err := s.col.GetTrip(ctx, id.TripID(req.Id), accountID)
+	if err != nil {
+		return nil, err
+	}
+
+	if req.Current != nil {
+		//TODO: 更新形成,计算价格
+	}
+
+	if req.GetEndTrip() {
+		//TODO: 结束形成
+	}
+	s.col.UpdateTrip(ctx,)
 }
