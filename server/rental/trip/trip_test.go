49ced43fe7b1bcd592566f83c314840a75b37d57
Ge-go <821230693@qq.com>
2022-09-27 21:21:49 +0800

    完成createtrip test,但尚未在本地测试(因为本地服务暂时不支持),接入前端

--- server/rental/trip/trip_test.go
+++ server/rental/trip/trip_test.go
@@ -1,1 +1,65 @@
 package trip
+
+import (
+	"context"
+	rentalpb "coolcar/rental/api/gen/v1"
+	"coolcar/rental/trip/client/poi"
+	"coolcar/rental/trip/dao"
+	"coolcar/shared/id"
+	mongotesting "coolcar/shared/mongo/testing"
+	"coolcar/shared/server"
+	"fmt"
+	"testing"
+)
+
+func TestService_CreateTrip(t *testing.T) {
+	ctx := context.Background()
+	mc, err := mongotesting.NewClient(ctx)
+	if err != nil {
+		t.Fatalf("cannot connect mongodb:%v", err)
+	}
+
+	m := dao.NewMongo(mc.Database("coolcar"))
+
+	logger, err := server.NewZapLogger()
+	if err != nil {
+		fmt.Errorf("cannot new zap logger:%v", err)
+	}
+	req := &rentalpb.CreateTripReq{
+		CarId: "car1",
+		Start: &rentalpb.Location{
+			Latitude:  32.123,
+			Longitude: 114.2525,
+		},
+	}
+
+	s := &Service{
+		ProfileManager: &profileManager{},
+		Log:            logger,
+		Mongo:          m,
+		CarManager:     &carManager{},
+		POIManager:     &poi.Manager{},
+	}
+	s.ProfileManager
+}
+
+type profileManager struct {
+	iID id.Identity
+	err error
+}
+
+func (p *profileManager) Verify(ctx context.Context, id id.AccountID) (id.Identity, error) {
+	return p.iID, p.err
+}
+
+type carManager struct {
+	err error
+}
+
+func (c *carManager) Verify(ctx context.Context, id id.CarID) error {
+	return c.err
+}
+
+func (c *carManager) Unlock(ctx context.Context, id id.CarID) error {
+	return c.err
+}
