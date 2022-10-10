c7b96d298aff5b4bbf07f88d6aa46f81e3e910b2
Ge-go <821230693@qq.com>
2022-10-10 11:16:58 +0800

    server service 开发完成,但接入模拟数据还未成功

--- wx/miniprogram/pages/index/index.ts
+++ wx/miniprogram/pages/index/index.ts
@@ -1,15 +1,30 @@
+import { IAppOption } from "../../appoption"
 import { CarService } from "../../service/car"
 import { ProfileService } from "../../service/profile"
 import { rental } from "../../service/proto_gen/rental/rental_pb"
 import { TripService } from "../../service/trip"
 import { routing } from "../../utils/routing"
 
+interface Marker {
+  iconPath: string
+  id: number
+  latitude: number
+  longitude: number
+  width: number
+  height: number
+}
+
+const defaultAvatar = '/resources/car.png'
+const initialLat = 29.761267625855936
+const initialLng = 121.87264654736123
+
 Page({
   isPageShowing: false,
   socket: undefined as WechatMiniprogram.SocketTask | undefined,
+
   data: {
-    avatarURL: '',  //头像url
-    setting: {  //map setting
+    avatarURL: '',
+    setting: {
       skew: 0,
       rotate: 0,
       showLocation: true,
@@ -26,63 +41,21 @@ Page({
       enableTraffic: false,
     },
     location: {
-      latitude: 31,
-      longitude: 120
+      latitude: initialLat,
+      longitude: initialLng,
     },
-    scale: 10,
-    markers: [
-      {
-        iconPath: "/resources/car.png",
-        id: 0,
-        latitude: 23.099994,
-        longitude: 113.324520,
-        joinCluster: true, // 指定了该参数才会参与聚合
-        width: 20,
-        height: 20,
-      },
-      {
-        iconPath: "/resources/car.png",
-        id: 1,
-        latitude: 23.099994,
-        longitude: 114.324520,
-        joinCluster: true, // 指定了该参数才会参与聚合
-        width: 20,
-        height: 20,
-      },
-      {
-        iconPath: "/resources/car.png",
-        id: 2,
-        latitude: 29.756825521115363,
-        longitude: 121.87222114786053,
-        joinCluster: true, // 指定了该参数才会参与聚合
-        width: 20,
-        height: 20,
-      },
-    ],
+    scale: 16,
+    markers: [] as Marker[],
   },
 
-  async onLoad() {  //目前能力只有手动拉起头像
-    this.socket = CarService.subscribe(
-      msg => {
-        console.log(msg)
-      }
-    )
-
-    wx.getUserProfile({
-      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res.userInfo.avatarUrl)
-        this.setData({
-          avatarURL: res.userInfo.avatarUrl,
-        })
-      }
+  async onLoad() {
+    const userInfo = await getApp<IAppOption>().globalData.userInfo
+    this.setData({
+      avatarURL: userInfo.avatarUrl,
     })
   },
 
   onMyLocationTap() {
-    //自动关闭socket
-    this.socket?.close({
-    })
     wx.getLocation({
       type: 'gcj02',
       success: res => {
@@ -90,54 +63,43 @@ Page({
           location: {
             latitude: res.latitude,
             longitude: res.longitude,
-          }
+          },
         })
-      },
+      }, 
       fail: () => {
         wx.showToast({
           icon: 'none',
-          title: '请勿频繁查询定位'
+          title: '请前往设置页授权',
         })
       }
     })
   },
 
-  //处理扫码动作
-  async onScanClicked() {
-    //验证当前是否已经有行程在进行
-    // const trips = await TripService.GetTrips(rental.v1.TripStatus.IN_PROGRESS)
-
-    // if (((trips.trips?.length) || 0) > 0) {
-    //   await this.selectComponent('#tripModal').showModal()
-    //   wx.navigateTo({
-    //     url: routing.drving({
-    //       trip_id: trips.trips![0].id!,
-    //     })
-    //   })
-    //   return
-    // }
-
+  async onScanTap() {
+    const trips = await TripService.GetTrips(rental.v1.TripStatus.IN_PROGRESS)
+    if ((trips.trips?.length || 0) > 0) {
+      await this.selectComponent('#tripModal').showModal()
+      wx.navigateTo({
+        url: routing.drving({
+          trip_id: trips.trips![0].id!,
+        }),
+      })
+      return
+    }
     wx.scanCode({
       success: async () => {
-        //TODO: 模拟获取car_id  为了方便后续页面接入(是否还需要再包装)
-        const carID = '633fe74af1c33f573d512450'
+        // TODO: get car id from scan result
+        const carID='60af01e5a21ead3dccbcd1d8'
         const lockURL = routing.lock({
-          car_id: carID
+          car_id: carID,
         })
-
         const prof = await ProfileService.getProfile()
-
-        // 判断是否已经验证过驾驶证信息
         if (prof.identityStatus === rental.v1.IdentityStatus.VERIFIED) {
           wx.navigateTo({
             url: lockURL,
           })
         } else {
           await this.selectComponent('#licModal').showModal()
-          this.setData({
-            showModal: true,
-          })
-
           wx.navigateTo({
             url: routing.register({
               redirectURL: lockURL,
@@ -145,64 +107,100 @@ Page({
           })
         }
       },
-      fail: res => {
-        console.log(res)
-        //针对错误特定处理
-        this.detailErrMsg(res)
-      },
+      fail: console.error,
     })
   },
 
-  detailErrMsg(res: WechatMiniprogram.GeneralCallbackResult) {
-    if (res.errMsg === "scanCode:fail") {  //special detail
-      wx.showToast({
-        icon: 'none',
-        title: '请扫描正确二维码',
+  onShow() {
+    this.isPageShowing = true;
+    if (!this.socket) {
+      this.setData({
+        markers: []
+      }, () => {
+        this.setupCarPosUpdater()
+      })
+    }
+  },
+  
+  onHide() {
+    this.isPageShowing = false;
+    if (this.socket) {
+      this.socket.close({
+        success: () => {
+          this.socket = undefined
+        }
       })
     }
   },
 
-  onShow() {
-    this.isPageShowing = true
-  },
-
-  onHide() {
-    this.isPageShowing = false
-  },
-
-  onMyTripsTip() {
+  onMyTripsTap() {
     wx.navigateTo({
       url: routing.mytrips(),
     })
   },
 
-  moveCars() {  //测试让车跑动
+  setupCarPosUpdater() {
     const map = wx.createMapContext("map")
-    const dest = {
-      latitude: 23.099994,
-      longitude: 113.324520,
+    const markersByCarID = new Map<string, Marker>()
+    let translationInProgress = false
+    const endTranslation = () => {
+      translationInProgress = false
     }
-
-    const moveCar = () => {
-      dest.latitude += 0.1
-      dest.longitude += 0.1
-      map.translateMarker({
-        destination: {
-          latitude: dest.latitude,
-          longitude: dest.longitude,
-        },
-        markerId: 0,
-        autoRotate: false,
-        rotate: 0,
-        duration: 5000,
-        animationEnd: () => {
-          if (this.isPageShowing) {
-            moveCar()
-          }
+    this.socket = CarService.subscribe(car => {
+      if (!car.id || translationInProgress || !this.isPageShowing) {
+        console.log('dropped')
+        return
+      }
+      const marker = markersByCarID.get(car.id)
+      if (!marker) {
+        // Insert new marker.
+        const newMarker: Marker = {
+          id: this.data.markers.length,
+          iconPath: car.car?.driver?.avatarUrl || defaultAvatar,
+          latitude: car.car?.position?.latitude || initialLat,
+          longitude: car.car?.position?.longitude || initialLng,
+          height: 20,
+          width: 20,
         }
-      })
-    }
+        markersByCarID.set(car.id, newMarker)
+        this.data.markers.push(newMarker)
+        translationInProgress = true
+        this.setData({
+          markers: this.data.markers,
+        }, endTranslation)
+        return
+      }
 
-    moveCar()
+      const newAvatar = car.car?.driver?.avatarUrl || defaultAvatar
+      const newLat = car.car?.position?.latitude || initialLat
+      const newLng = car.car?.position?.longitude || initialLng
+      if (marker.iconPath !== newAvatar) {
+        // Change iconPath and possibly position.
+        marker.iconPath = newAvatar
+        marker.latitude = newLat
+        marker.longitude = newLng
+        translationInProgress = true
+        this.setData({
+          markers: this.data.markers,
+        }, endTranslation)
+        return
+      }
+
+      if (marker.latitude !== newLat || marker.longitude !== newLng) {
+        // Move marker.
+        translationInProgress = true
+        map.translateMarker({
+          markerId: marker.id,
+          destination: {
+            latitude: newLat,
+            longitude: newLng,
+          },
+          autoRotate: false,
+          rotate: 0,
+          duration: 80,
+          animationEnd: endTranslation,
+        })
+      }
+    })
   },
 })
